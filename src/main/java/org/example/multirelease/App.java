63bab7092ad50417a094175b567f8f2fe7b63784
orionll <orionllmain@gmail.com>
2018-05-29 17:44:28 +0700

    Added pom.xml, App.java and module-info.class

---
+++ src/main/java/org/example/multirelease/App.java
@@ -0,0 +1,10 @@
+package org.example.multirelease;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
